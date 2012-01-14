38c7d9fddd288dbe287805751158808889b0fb08
Shrikant-Sharat <shrikantsharat.k@gmail.com>
2012-01-14 16:36:53 +0530

    Raise version to 0.0.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject classypants "0.0.1"
+(defproject classypants "0.0.2"
   :description "A path-like environment variable editor"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
